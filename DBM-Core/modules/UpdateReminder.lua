79840a6f31f3f25b6eeb4023088b386fa5ace904
YggdrasilWotLK <157967573+YggdrasilWotLK@users.noreply.github.com>
2026-09-19 22:54:10 +0200

    Point website link to new URL

--- DBM-Core/modules/UpdateReminder.lua
+++ DBM-Core/modules/UpdateReminder.lua
@@ -74,11 +74,11 @@ local function CreateOurFrame()
 end
 
 function DBM:ShowUpdateReminder(newVersion, newRevision, text, url)
-	urlText = url or "https://github.com/DeadlyBossMods/DeadlyBossMods/wiki"
+	urlText = url or "https://github.com/YggdrasilWotLK/DBM-Yggdrasil"
 	if not frame then
 		CreateOurFrame()
 	end
-	editBox:SetText(url or "https://github.com/DeadlyBossMods/DeadlyBossMods/wiki")
+	editBox:SetText(url or "https://github.com/YggdrasilWotLK/DBM-Yggdrasil")
 	editBox:HighlightText()
 	frame:Show()
 	if newVersion then
